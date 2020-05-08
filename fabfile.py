97172cdbfda43cc322068cc00dacc07dd506c72e
zeeta <zeetazhu@gmail.com>
2020-05-08 23:30:27 +0800

    fix bug

--- fabfile.py
+++ fabfile.py
@@ -44,5 +44,7 @@ def deploy(c):
 
     # 重新启动应用
     with c.cd(supervisor_conf_path):
+        c.run('supervisord -c supervisord.conf')
+        c.run('source environment.ini')
         cmd = 'supervisorctl start {}'.format(supervisor_program_name)
         c.run(cmd)
