111a067353f5e5c1c45f6d06ffe71708207b25d3
zeeta <zeetazhu@gmail.com>
2020-05-09 00:22:22 +0800

    fix

--- fabfile.py
+++ fabfile.py
@@ -38,8 +38,6 @@ def deploy(c):
 
     # 安装依赖，迁移数据库，收集静态文件
     with c.cd(project_root_path):
-        c.run('source ~/etc/environment.ini')
-        print("DJANGO_SECRET_KEY", os.environ['DJANGO_SECRET_KEY'])
         c.run('pipenv install --deploy --ignore-pipfile')
         c.run('pipenv run python manage.py migrate')
         c.run('pipenv run python manage.py collectstatic --noinput')
